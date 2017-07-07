ec0bbccafe70dc3e0bd7839face10fd290b622d0
Tillmann Heigel <heigel@tillmannheigel.de>
2017-07-07 09:23:12 +0200

    add SLf4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 import crawler.collector.OfferCollector;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Starting");
         OfferCollector offers = new OfferCollector("https://www.starcar.de/specials/kostenlos-mieten/", ".togglelist-content-item");
-        offers.collectOffers();
+        String[] starCarLinks = offers.collectOffers();
+        log.info("Starcar Offers: {}", Arrays.toString(starCarLinks));
     }
 }
--- src/main/java/models/StarcarModel.java
+++ src/main/java/models/StarcarModel.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class StarcarModel {
+
     String car;
     String group;
     String type;
