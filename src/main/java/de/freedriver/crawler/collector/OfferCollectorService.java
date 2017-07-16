15bc4bedbe9f096feb189c163865b0cac8dc01e9
Tillmann Heigel <heigel@tillmannheigel.de>
2017-07-16 19:37:09 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 69560042d420f2578664e802243c8374b76c63b1, reversing
    changes made to 4f14198d8e8cb2b729d267946e7d71afa1814d05.

---
+++ src/main/java/de/freedriver/crawler/collector/OfferCollectorService.java
@@ -0,0 +1,41 @@
+package de.freedriver.crawler.collector;
+
+import de.freedriver.crawler.CrawlerService;
+import de.freedriver.service.KafkaMessengerService;
+import lombok.Data;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Data
+@Component
+public class OfferCollectorService {
+
+    @Autowired
+    KafkaMessengerService kafkaMessengerService;
+    @Autowired
+    CrawlerService crawlerService;
+
+    public HashSet<String> collectOffers(String url, String cssQuery) {
+        try {
+            Elements elements = crawlerService.crawlHtmlElements(url, cssQuery);
+            String[] array = elements.stream()
+                    .map(this::extractLinks)
+                    .toArray(String[]::new);
+            return new HashSet<>(Arrays.asList(array));
+        } catch (IOException e) {
+            kafkaMessengerService.sendToExceptionTopic(this.toString(), e.getMessage());
+            e.printStackTrace();
+        }
+        return new HashSet<>();
+    }
+
+    private String extractLinks(Element element) {
+        return element.select("a[href]").attr("href");
+    }
+}
