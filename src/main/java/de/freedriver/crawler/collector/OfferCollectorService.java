592dc598fdcc782deccef59da62640d7e8d98d34
Tillmann Heigel <heigel@tillmannheigel.de>
2017-07-22 07:52:02 +0200

    WireMock: Add Dependency

--- src/main/java/de/freedriver/crawler/collector/OfferCollectorService.java
+++ src/main/java/de/freedriver/crawler/collector/OfferCollectorService.java
@@ -2,6 +2,7 @@ package de.freedriver.crawler.collector;
 
 import de.freedriver.crawler.CrawlerService;
 import de.freedriver.service.KafkaMessengerService;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 public class OfferCollectorService {
 
@@ -27,6 +29,7 @@ public class OfferCollectorService {
                     .collect(Collectors.toCollection(HashSet::new));
         } catch (IOException e) {
             kafkaMessengerService.sendToExceptionTopic(this.toString(), e.getMessage());
+            log.error("Unable to collect offers {}", e.getMessage(), e);
             e.printStackTrace();
         }
         return new HashSet<>();
