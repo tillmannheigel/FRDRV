66c46a14887c30f7ca7aca6b63b3ad0a548022cf
Tillmann Heigel <tillmannheigel@gmail.com>
2017-11-06 14:26:48 +0100

    Enable MongoRepository via JavaConfig

--- src/main/java/de/freedriver/config/Config.java
+++ src/main/java/de/freedriver/config/Config.java
@@ -3,14 +3,18 @@ package de.freedriver.config;
 import com.mongodb.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@EnableMongoRepositories
 class Config {
+
     /*
-  * Use the standard Mongo driver API to create a com.mongodb.MongoClient instance.
-  */
+    * Use the standard Mongo driver API to create a com.mongodb.MongoClient instance.
+    */
     @Bean
     public MongoClient mongoClient() {
         return new MongoClient("localhost");
     }
+
 }
