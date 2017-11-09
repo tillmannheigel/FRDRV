e976f96a2db0c556593532c28efa624342a5010a
Tillmann Heigel <heigel@tillmannheigel.de>
2017-11-09 21:42:15 +0100

    Fix OffersRepository method

--- src/main/java/de/freedriver/config/Config.java
+++ src/main/java/de/freedriver/config/Config.java
@@ -4,28 +4,26 @@ import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import de.freedriver.repositories.OffersRepository;
 
 @Configuration
-@EnableMongoRepositories
-class Config extends AbstractMongoConfiguration{
+class Config {
 
-    @Override
-    protected String getDatabaseName() {
-        return "driver-store";
+    @Bean
+    public MongoDbFactory mongoDbFactory() throws Exception {
+        return new SimpleMongoDbFactory(new MongoClient(), "yourdb");
     }
 
-    @Override
-    public Mongo mongo() throws Exception {
-        return new Mongo();
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+       return new MongoTemplate(mongoDbFactory());
     }
 
-    @Override
-    protected String getMappingBasePackage() {
-        return "com.oreilly.springdata.mongodb";
-    }
 
 }
--- src/main/java/de/freedriver/repositories/OffersRepository.java
+++ src/main/java/de/freedriver/repositories/OffersRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface OffersRepository extends MongoRepository<Offer, String>, OffersRepositoryCustom {
 
-    List<Offer> findOffersByStartAt();
+    List<Offer> findOffersByStartStation();
 }
