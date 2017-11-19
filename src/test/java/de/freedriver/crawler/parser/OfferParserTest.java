f7171d5154f5a7c704586fc825a12de9994d43a2
Tillmann Heigel <heigel@tillmannheigel.de>
2017-11-19 18:15:00 +0100

    Fix OfferParserTest

--- src/test/java/de/freedriver/crawler/parser/OfferParserTest.java
+++ src/test/java/de/freedriver/crawler/parser/OfferParserTest.java
@@ -1,8 +1,8 @@
 package de.freedriver.crawler.parser;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
+import de.freedriver.crawler.CrawlerService;
+import de.freedriver.models.Offer;
+import de.freedriver.repositories.OffersRepository;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
@@ -12,23 +12,26 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import de.freedriver.crawler.CrawlerService;
-import de.freedriver.models.Offer;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OfferParserTest extends Mockito {
 
+    static String MOCK_URL = "http://mock.url/?offer=123";
     @InjectMocks
-    OfferParser offerParser;
+    private OfferParser offerParser;
     @Mock
-    CrawlerService crawler;
+    private CrawlerService crawler;
+    @Mock
+    private OffersRepository offersRepository;
 
     @Test
     public void parseEmptyOffer() throws Exception {
         //given
         Elements elements = new Elements(0);
         //when
-        Offer offer = offerParser.parseOffer("mockUrl", "mockCss");
+        Offer offer = offerParser.parseOffer(MOCK_URL, "mockCss");
         //then
         assertNull(offer);
     }
@@ -36,11 +39,10 @@ public class OfferParserTest extends Mockito {
     @Test
     public void parseExistingOffer() throws Exception {
         //given
-        String mockUrl = "mockUrl";
         String mockCss = "mockCss";
         Element element = mock(Element.class);
         Elements elements = mock(Elements.class);
-        when(crawler.crawlHtmlElements(mockUrl, mockCss)).thenReturn(elements);
+        when(crawler.crawlHtmlElements(MOCK_URL, mockCss)).thenReturn(elements);
         when(elements.size()).thenReturn(5);
         when(element.html()).thenReturn("mockHtml");
         for (int i = 0; i < 6; i++) {
@@ -48,7 +50,7 @@ public class OfferParserTest extends Mockito {
         }
 
         //when
-        Offer offer = offerParser.parseOffer(mockUrl, mockCss);
+        Offer offer = offerParser.parseOffer(MOCK_URL, mockCss);
 
         //then
         assertNotNull(offer);
