37da052eb3243e4e9e3e7e266d988ed32fac9919
Tillmann Heigel <heigel@tillmannheigel.de>
2017-11-22 15:16:20 +0100

    Remove unused imports

--- src/test/java/de/freedriver/annotations/Skip.java
+++ src/test/java/de/freedriver/annotations/Skip.java
@@ -4,10 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.ArrayList;
-import java.util.List;
-
-import de.freedriver.models.Offer;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
